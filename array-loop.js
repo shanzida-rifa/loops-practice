93dac3b22cb33737f207f1e57dc06504082a3bf3
Shanzida-Rifa <rifashanzida@gmail.com>
2022-09-13 21:43:02 +0600

    array-loop

---
+++ array-loop.js
@@ -0,0 +1,7 @@
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+var numbers = [45, 87, 89, 56, 32, 51, 25, 56, 78];
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
---
+++ for.js
@@ -0,0 +1,7 @@
+var roastGiven = 0;
+for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+    console.log('Roast den,Please!!');
+}
+for (var i = 1; i < 10; i++) {
+    console.log(i);
+}
