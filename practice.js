faeafd6c808eda7e4722e178c2703df9caf2037b
Shanzida-Rifa <rifashanzida@gmail.com>
2022-09-15 11:27:16 +0600

    assesmentt

---
+++ practice.js
@@ -0,0 +1,29 @@
+// var dis = 'Ajke amar mon valo nei';
+// for (var i = 581; i <= 623; i++) {
+//     if (i % 2 == 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+var learn = ['html', 'css', 'bootstrap', 'js', null];
+// console.log(learn);
+// for (var i = 0; i <= learn.length; i++) {
+//     console.log(learn[i]);
+// }
+// var i = 0;
+// while (i <= learn.length) {
+//     console.log(learn[i]);
+//     i++;
+// }
+// for(var i=30;i<=86;i++){
+//     if(i>44){
+//         break;
+//     }
+//     console.log(i);
+// }
+var bookPrice=['200','456','87','900','123','125','876'];
+for (var i=0; i<=bookPrice.length;i++){
+    if(bookPrice[i]<200){
+        console.log(bookPrice[i]);
+    }
+}
---
+++ reverse.js
@@ -0,0 +1,8 @@
+var num = 10;
+while (num >= 1) {
+    console.log(num);
+    num--;
+}
+for (var i = 5; i >= 1; i--) {
+    console.log(i);
+}
